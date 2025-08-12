4f32dd572c3bde5f9d86b1f74d9378a5cd1b5328
HP-Esther <omikunleesther@gmail.com>
2025-08-12 15:56:29 +0100

    Third task

--- ProjectEulerTasks/Logic/EulerTasks.cs
+++ ProjectEulerTasks/Logic/EulerTasks.cs
@@ -54,7 +54,15 @@ namespace ProjectEulerTasks.Main.Logic
             return sum;
         }
 
+        /*
+         * The prime factors of 13195 are 5, 7, 13 and 29
+            What is the largest prime factor of the number 600851475143?
+        */
         
+        internal static int LargestPrimeFactor()
+        {
+            return 0;
+        }
         public EulerTasks()
         {
         }

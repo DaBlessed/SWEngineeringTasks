1ec670a97b12cb22e176bbec0618c699efc44b46
HP-Esther <omikunleesther@gmail.com>
2025-08-12 01:43:11 +0100

    Add project files.

---
+++ ProjectEulerTasks/Logic/NumberUtilities.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEulerTasks.Main.Logic
+{
+    public class NumberUtilities
+    {
+        public NumberUtilities() { }
+        internal static int GetFibonacciValue(int n)
+        {
+            /*
+            int fibValue;
+          // var fibStore = new Dictionary<int, int>();
+
+            if (n < 0)
+            {
+                return -1;
+            }
+
+            else if (n == 0 && n < 2)
+            {
+                return n;
+            }
+
+            else 
+            {
+                fibValue = GetFibonacciValue(n - 1) + GetFibonacciValue(n - 2);
+              //  fibStore.Add(n, fibValue);
+                return fibValue;
+            }
+            */
+
+            int a, b, temp;
+            a = 0; b = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                temp = a;
+                a = b;
+                b = temp + b;
+            }
+
+            return a;
+
+        }
+    }
+}
---
+++ ProjectEulerTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using ProjectEulerTasks.Main.Logic;
+
+var sumOfMultiplesOf3And5 = EulerTasks.SumOfMultiplesOf3And5();
+var sumOfEvenFibonacciValues = EulerTasks.SumOfEvenFibonacciNumbers();
+
+//Console.WriteLine(sumOfMultiplesOf3And5);
+Console.WriteLine(sumOfEvenFibonacciValues);
